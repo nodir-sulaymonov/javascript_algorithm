2d1a8fe9f1e4e3f861f2b70b11112a7dad8def69
nodir-sulaymonov <s.nodir9494@gmail.com>
2020-06-18 11:47:53 +0300

    finish 10 tasks

---
+++ code_camp/BasicJavaSript.js
@@ -0,0 +1,66 @@
+/*1 Assignment OperatorPassed
+var a;
+var b = 2;
+a = 7;
+b = a;
+
+ 2.
+var myVar = 0;
+var a = 9;
+ */
+
+/* 3 Case Sensitivity
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+ */
+
+/*4 Variable Camel Case
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+*/
+
+/*5 Increment
+* var myVar = 87;
+
+myVar++;
+ */
+
+/*6 Remainder
+ var remainder = 11 % 3;
+ */
+
+/*7 Compound Assignment
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;*/
+
+/* 8 Literal Quotes
+ var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+ */
+
+/*9 Single quotes
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+*/
+
+/*10 Escape Sequences
+var myStr;
+myStr = "FirstLine\n\t\\SecondLine\nThirdLine";*/
